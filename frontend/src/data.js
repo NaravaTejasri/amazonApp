40b90e94218071dfec7ffdfe1103de8393a3a81c
TejasriNarava <tejasrisony17@gmail.com>
2021-07-14 17:46:15 +0200

    first commit

---
+++ frontend/src/data.js
@@ -0,0 +1,67 @@
+const data = {
+    products: [
+        {
+            _id: '1',
+            name: 'Nike Slim Shirt',
+            category: 'shirt',
+            image: '/images/girl.jpeg',
+            price: 120,
+            rating: 4.5,
+            description: 'High quality product'
+
+        },
+        {
+            _id: '2',
+            name: 'Only Slim Shirt',
+            category: 'shirt',
+            image: '/images/shirt1.jpg',
+            price: 150,
+            rating: 4.5,
+            description: 'High quality product'
+
+        },
+        {
+            _id: '3',
+            name: 'Lee cooper Slim Shirt',
+            category: 'shirt',
+            image: '/images/shirt2.jpg',
+            price: 110,
+            rating: 4.5,
+            description: 'High quality product'
+
+        },
+        {
+            _id: '4',
+            name: 'Harpa Slim Shirt',
+            category: 'shirt',
+            image: '/images/shirt3.jpg',
+            price: 100,
+            rating: 4.5,
+            description: 'High quality product'
+
+        },
+        {
+            _id: '5',
+            name: 'Allen solly Shirt',
+            category: 'shirt',
+            image: '/images/shirt4.jpg',
+            price: 150,
+            rating: 4.5,
+            description: 'High quality product'
+
+        },
+        {
+            _id: '6',
+            name: 'pepe jeans Slim Shirt',
+            category: 'shirt',
+            image: '/images/shirt5.jpg',
+            price: 160,
+            rating: 4.5,
+            description: 'High quality product'
+
+        },
+       
+    ],
+};
+
+export default data;
