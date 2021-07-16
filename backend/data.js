8c8afc5ac97e6e6826967d4f907293ee0fc7a36f
TejasriNarava <tejasrisony17@gmail.com>
2021-07-16 11:41:59 +0200

    committing backend folder

---
+++ backend/data.js
@@ -0,0 +1,79 @@
+const data = {
+    products: [
+        {
+            _id: '1',
+            name: 'Nike Slim Shirt',
+            category: 'shirt',
+            image: '/images/girl.jpeg',
+            price: 120,
+            countInStock: 20,
+            rating: 4.5,
+            numReviews: 20,
+            description: 'High quality product'
+
+        },
+        {
+            _id: '2',
+            name: 'Only Slim Shirt',
+            category: 'shirt',
+            image: '/images/shirt1.jpg',
+            price: 150,
+            countInStock: 30,
+            rating: 3.5,
+            numReviews: 10,
+            description: 'High quality product'
+
+        },
+        {
+            _id: '3',
+            name: 'Lee cooper Slim Shirt',
+            category: 'shirt',
+            image: '/images/shirt2.jpg',
+            price: 110,
+            countInStock: 0,
+            rating: 4.8,
+            numReviews: 15,
+            description: 'High quality product'
+
+        },
+        {
+            _id: '4',
+            name: 'Harpa Slim Shirt',
+            category: 'shirt',
+            image: '/images/shirt3.jpg',
+            price: 100,
+            countInStock: 10,
+            rating: 4.5,
+            numReviews: 30,
+            description: 'High quality product'
+
+        },
+        {
+            _id: '5',
+            name: 'Allen solly Shirt',
+            category: 'shirt',
+            image: '/images/shirt4.jpg',
+            price: 150,
+            countInStock: 10,
+            rating: 2.5,
+            numReviews: 5,
+            description: 'High quality product'
+
+        },
+        {
+            _id: '6',
+            name: 'pepe jeans Slim Shirt',
+            category: 'shirt',
+            image: '/images/shirt5.jpg',
+            price: 160,
+            countInStock: 15,
+            rating: 1.5,
+            numReviews: 3,
+            description: 'High quality product'
+
+        },
+       
+    ],
+};
+
+export default data;
