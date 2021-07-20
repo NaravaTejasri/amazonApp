42dc51a41f93387162efaf717df3a16116afe0b4
TejasriNarava <tejasrisony17@gmail.com>
2021-07-20 11:50:36 +0200

    created redux-store

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,49 +1,34 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React, { useEffect } from 'react';
 import Product from '../components/Product';
 import LoadingBox from '../components/LoadingBox';
 import MessageBox from '../components/MessageBox';
+import { useDispatch, useSelector } from 'react-redux';
+import { listProducts } from '../actions/productActions';
 
+ function HomeScreen() {
+  const dispatch = useDispatch();
+  const productList = useSelector((state) => state.productList);
+  const { loading, error, products } = productList;
 
-function HomeScreen() {
-   const [products, setProducts] = useState([]);
-   const [loading, setLoading] = useState(false);
-   const [error, setError] = useState(false);
-
-   useEffect(() => {
-      const fetchData = async() => {
-         try {
-            setLoading(true);
-            const { data } = await axios.get('/api/products');
-            setLoading(false);
-            setProducts(data);
-         }catch (err) {
-            setError(err.message);
-            setLoading(false);
-         }     
-      };
-      fetchData();   
-   }, []);
-    return(
-     <div>
+  useEffect(() => {
+    dispatch(listProducts());
+  }, [dispatch]);
+   return (
+      <div>
         {loading ? (
-            <LoadingBox></LoadingBox>
+          <LoadingBox></LoadingBox>
         ) : error ? (
-            <MessageBox varient="danger">{error}</MessageBox>
+          <MessageBox variant="danger">{error}</MessageBox>
         ) : (
-        <div className="row center">
-        {
-           products.map((product) => (
-           <Product key={product._id} product={product}/>
-        ))
-        }
-      
-       </div> 
+          <div className="row center">
+            {products.map((product) => (
+              <Product key={product._id} product={product}></Product>
+            ))}
+          </div>
         )}
-      
-    </div>
+      </div>
     );
-}
+  }
 
 export default HomeScreen;
 
@@ -63,3 +48,44 @@ export default HomeScreen;
 }
 
 export default HomeScreen; */
+
+
+
+/* const [products, setProducts] = useState([]);
+const [loading, setLoading] = useState(false);
+const [error, setError] = useState(false);
+
+useEffect(() => {
+   const fetchData = async() => {
+      try {
+         setLoading(true);
+         const { data } = await axios.get('/api/products');
+         setLoading(false);
+         setProducts(data);
+      }catch (err) {
+         setError(err.message);
+         setLoading(false);
+      }     
+   };
+   fetchData();   
+}, []);
+ return(
+  <div>
+     {loading ? (
+         <LoadingBox></LoadingBox>
+     ) : error ? (
+         <MessageBox varient="danger">{error}</MessageBox>
+     ) : (
+     <div className="row center">
+     {
+        products.map((product) => (
+        <Product key={product._id} product={product}/>
+     ))
+     }
+   
+    </div> 
+     )}
+   
+ </div>
+ );
+} */
