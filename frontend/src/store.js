65ec9439e6760d0ea164ec0e2026c1432740213b
TejasriNarava <tejasrisony17@gmail.com>
2021-08-25 11:12:29 +0200

    committing signin and signout

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,19 +1,29 @@
-import { createStore, compose, applyMiddleware, combineReducers } from 'redux';
-import thunk from 'redux-thunk';
-import { cartReducer } from './reducers/cartReducers';
-import { productDetailsReducer, productListReducer } from './reducers/productReducers';
+import { createStore, compose, applyMiddleware, combineReducers } from "redux";
+import thunk from "redux-thunk";
+import { cartReducer } from "./reducers/cartReducers";
+import {
+  productDetailsReducer,
+  productListReducer,
+} from "./reducers/productReducers";
+import { userSigninReducer } from "./reducers/userReducers";
 
 const initialState = {
-  cart : {
-    cartItems : localStorage.getItem('cartItems') 
-    ? JSON.parse(localStorage.getItem('cartItems'))
-    : [],
+  userSignin: {
+    userInfo: localStorage.getItem("userInfo")
+      ? JSON.parse(localStorage.getItem("userInfo"))
+      : null,
+  },
+  cart: {
+    cartItems: localStorage.getItem("cartItems")
+      ? JSON.parse(localStorage.getItem("cartItems"))
+      : [],
   },
 };
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
   cart: cartReducer,
+  userSignin: userSigninReducer,
 });
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
@@ -23,6 +33,3 @@ const store = createStore(
 );
 
 export default store;
-
-
-
